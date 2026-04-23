e885fc2a73e9b5dd75f5d0ecd2f4cc2def42ec4c
leonardo <leonardo.rodrigues06@escola.pr.gov.br>
2026-04-23 20:39:35 -0300

    atualizando e adicionando raizes,parenteses, potencia e deixando a calculadora mais estética

--- main.js
+++ main.js
@@ -6,7 +6,9 @@ function appendToDisplay(value) {
 function calculate() {
     const display = document.getElementById("display");
     try {
-        display.value = eval(display.value);
+        let expression = display.value.replace(/,/g, '');
+        let result = eval(expression);
+        display.value = Number(result).toLocaleString("en-US", { maximumFractionDigits: 10 });
     } catch (error) {
         display.value = "Erro";
     }
