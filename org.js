232d8fa1cd10a171bbe25e9841a87f221f03e89c
samoel da silva cardoso <samoel.cardoso@escola.pr.gov.br>
2026-04-24 11:39:19 -0300

    adicionei alguns arquivos e uma página para a velha calculadora

--- org.js
+++ org.js
@@ -25,7 +25,7 @@ switch (Defdocal) {
         console.log("Valor Presente - VP");
         console.log("Valor Futuro - VF");
 
-        let opcao = prompt("Qual cálculo deseja fazer?").toUpperCase();
+        let opcao = prompt("Qual cálculo deseja fazer?");
 
         switch (opcao) {
 
