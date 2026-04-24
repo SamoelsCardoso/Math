f014f8f86ee96b8358c22ee937342e87f9a30691
leonardo <leonardo.rodrigues06@escola.pr.gov.br>
2026-04-23 21:29:30 -0300

    otimização do .js para matematica mais avançada

--- main.js
+++ main.js
@@ -3,12 +3,19 @@ function appendToDisplay(value) {
     display.value += value;
 }
 
-function calculate() {
+function calcular() {
     const display = document.getElementById("display");
     try {
-        let expression = display.value.replace(/,/g, '');
-        let result = eval(expression);
-        display.value = Number(result).toLocaleString("en-US", { maximumFractionDigits: 10 });
+        let expressão = display.value.replace(/,/g, '');
+
+        expressão = expressão.replace(/\^/g, '**');
+        expressão = expressão.replace(/√\(/g, 'Math.sqrt(');
+        expressão = expressão.replace(/√(\d+(\.\d+)?)/g, 'Math.sqrt($1)');
+        expressão = expressão.replace(/%(\d+(\.\d+)?)/g, '/100*$1');
+        expressão = expressão.replace(/%/g, '/100');
+
+        let resultado = eval(expressão);
+        display.value = Number(resultado).toLocaleString("en-US", { maximumFractionDigits: 10 });
     } catch (error) {
         display.value = "Erro";
     }
